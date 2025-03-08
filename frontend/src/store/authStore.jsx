e8e6b56586d9c108c61d5187398c08f1b60c972a
Ade <joelayomide35@gmail.com>
2025-03-08 12:29:31 +0100

    FIXED THE ENV FILE

--- frontend/src/store/authStore.jsx
+++ frontend/src/store/authStore.jsx
@@ -2,7 +2,8 @@ import { create } from "zustand";
 import axios from "axios";
 import toast, { Toaster } from 'react-hot-toast';
 
-const API_URL ="https://waka-agent.vercel.app/api/auth"
+
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
--- frontend/src/store/editStore.jsx
+++ frontend/src/store/editStore.jsx
@@ -2,7 +2,8 @@ import { create } from "zustand";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
-const API_URL = "https://waka-agent.vercel.app/api/edit"
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/edit" : "/api/edit";
+
 
 axios.defaults.withCredentials = true;
 
