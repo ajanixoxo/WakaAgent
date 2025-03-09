534dbf2ce676677f8dd4497445dd034aef7eae3a
Ade <joelayomide35@gmail.com>
2025-03-09 07:06:18 +0100

    let try it one more time

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const __dirname= path.resolve()
 
-app.use(cors({origin: ['http://localhost:5173' ], credentials:true}))
+app.use(cors({origin: ['http://localhost:5173', ['https://www.trekkingagent.com'] ], credentials:true}))
 app.use(express.json())//allows us to parse incoming requesit
 app.use(cookieParser())//allows us to parse incoming cookies
 app.use("/api/auth", authRoutes)
