e57688925fdbae05eb5800af4a8a6749d4a3b00f
Ade <joelayomide35@gmail.com>
2025-03-12 15:21:47 +0100

    deployed

--- backend/index.js
+++ backend/index.js
@@ -37,10 +37,10 @@ app.use("/api/edit", editRoutes);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "backend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "backend", "dist", "index.html"));
   });
 }
 
